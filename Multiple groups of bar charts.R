a9535fb911c5c97ed0948a502070e764a5017d7a
shuaima1991 <88414847+shuaima1991@users.noreply.github.com>
2021-11-13 11:16:43 +0800

    Update Multiple groups of bar charts.R

--- Multiple groups of bar charts.R
+++ Multiple groups of bar charts.R
@@ -3,8 +3,8 @@ library(ggpubr)
 library(circlize)
 library(RColorBrewer)
 pFilter=0.05
-setwd("D:/Rcode/文章思路/细胞焦亡相关lncRNA/46lncRNA census cluster/46基因在肿瘤级别中的表达")                           #设置工作目录
-rt=read.table("46lncRNA输入文件.txt",sep="\t",header=T,check.names=F)    #读取文件
+setwd("")                           #璁剧疆宸ヤ綔鐩綍
+rt=read.table("46lncRNA input.txt",sep="\t",header=T,check.names=F)   
 rt=rt[!duplicated(rt$ID), ]
 rownames(rt)=rt[,1]
 rt=rt[,-1]
@@ -32,14 +32,14 @@ for(i in colnames(data[,1:(ncol(data)-2)])){
 }
 write.table(outTab,file="data.txt",sep="\t",row.names=F,quote=F)
 
-#绘制箱型图
-data=read.table("data.txt",sep="\t",header=T,check.names=F)       #读取箱线图输入文件
+
+data=read.table("data.txt",sep="\t",header=T,check.names=F)       
 data$Subtype=factor(data$Subtype, levels=c("G4","G3","G2"))
 p=ggboxplot(data, x="gene", y="expression", color = "Subtype",
             ylab="Expression value",
             xlab="",
             palette = c("red","#00BFFF","#F4A460") )
 p=p+rotate_x_text(45)
-pdf(file="nomal tumor2.pdf",width=10,height=6)                          #输出图片文件
+pdf(file="nomal tumor2.pdf",width=10,height=6)                         
 p+stat_compare_means(aes(group=Subtype),symnum.args=list(cutpoints = c(0, 0.001, 0.01, 0.05, 1), symbols = c("***", "**", "*", "ns")),label = "p.signif")
 dev.off()
